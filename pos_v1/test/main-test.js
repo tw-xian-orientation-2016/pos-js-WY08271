530aa16f7c6aea72074d72dba8d1ce5160f8e98c
WY08271 <wy08271@outlook.com>
2016-03-04 17:10:43 +0800

    add receipt() function

--- pos_v1/test/main-test.js
+++ pos_v1/test/main-test.js
@@ -185,3 +185,62 @@ describe('getReceiptItems()', function () {
     expect(results).toEqual(expectResult);
   });
 });
+
+describe('receipt()', function () {
+  var receiptItems;
+
+  beforeEach(function () {
+    receiptItems = [{
+      cartItem: {
+        item: {
+          barcode: 'ITEM000001',
+          name: '雪碧',
+          unit: '瓶',
+          price: 3.00
+        },
+        count: 5
+      },
+      subTotal: 12,
+      saveSubTotal: 3
+    }, {
+      cartItem: {
+        item: {
+          barcode: 'ITEM000003',
+          name: '荔枝',
+          unit: '斤',
+          price: 15.00
+        },
+        count: 2
+      },
+      subTotal: 30,
+      saveSubTotal: 0
+    }, {
+      cartItem: {
+        item: {
+          barcode: 'ITEM000005',
+          name: '方便面',
+          unit: '袋',
+          price: 4.50
+        },
+        count: 3
+      },
+      subTotal: 9,
+      saveSubTotal: 4.5
+    }];
+  });
+
+  it('can receipt correct result', function () {
+    var result = receipt(receiptItems);
+    var expectResult =
+      '***<没钱赚商店>收据***\n' +
+      '名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)\n' +
+      '名称：荔枝，数量：2斤，单价：15.00(元)，小计：30.00(元)\n' +
+      '名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)\n' +
+      '----------------------\n' +
+      '总计：51.00(元)\n' +
+      '节省：7.50(元)\n' +
+      '**********************';
+
+    expect(result).toEqual(expectResult);
+  });
+});
